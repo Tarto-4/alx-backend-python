437f96f65dea220926852fab1f058293a23d3e68
Thato Mongwe <thatomongwe47@gmail.com>
2024-10-10 14:15:35 +0200

    refractor code

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+'''Task 0's module.
+'''
+
+
 def add(a: float, b: float) -> float:
-    """Returns the sum of two floats"""
+    '''Adds two floating-point numbers.
+    '''
     return a + b
--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+'''Task 3's module.
+'''
+
 
 def to_str(n: float) -> str:
-    """Converts a float to a string."""
+    '''Converts a floating-point number to a string.
+    '''
     return str(n)
